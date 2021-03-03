18196035525e0d745aa2e26c58937fdb0cdde0d3
oleg gugunava <oleggugunava@gmail.com>
2021-03-04 03:11:30 +0400

    Added C#

---
+++ C#/Algorithms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[]{1,2,3,4 };
+            ReverseArray reverseArray = new ReverseArray(n);
+            int[] reversed = reverseArray.GetReverseArray();
+            foreach(var item in reversed){
+                Console.Write($"{item}, ");
+            }
+
+        }
+    }
+}
---
+++ C#/Algorithms/ReverseArray.cs
@@ -0,0 +1,27 @@
+namespace Algorithms
+{
+    public class ReverseArray
+    {
+        int[] nums;
+        private int startIndex;
+        private int endIndex;
+        public ReverseArray(int[] nums)
+        {
+            this.nums = nums;
+            startIndex = 0;
+            endIndex = nums.Length - 1;
+        }
+
+        public int[] GetReverseArray()
+        {
+            while(endIndex > startIndex){
+                int start = nums[startIndex];
+                nums[startIndex] = nums[endIndex];
+                nums[endIndex] = start;
+                startIndex++;
+                endIndex--;
+            }
+            return nums;
+        }
+    }
+}
