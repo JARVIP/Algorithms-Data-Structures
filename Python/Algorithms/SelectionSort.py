247af339149c275d9a9aebe42f49b2a24e2c0909
oleg gugunava <oleggugunava@gmail.com>
2021-03-19 16:07:42 +0400

    selection sort

---
+++ Python/Algorithms/SelectionSort.py
@@ -0,0 +1,18 @@
+
+def selection_sort(nums):
+
+    for i in range(len(nums) - 1):
+        index = i
+
+        for j in range(i, len(nums)):
+            if nums[j] < nums[index]:
+                index = j
+
+        if index != i:
+            nums[index], nums[i] = nums[i], nums[index]
+
+
+if __name__ == "__main__":
+    n = [2, 23, 2, -1, 32, 0, 44, 12, 4]
+    selection_sort(n)
+    print(n)
